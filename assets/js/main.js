40f28d81d5dcf931c598f2fa36fccc7e93e9f016
yutinghCW <33119123+yutinghCW@users.noreply.github.com>
2019-07-07 19:53:44 +0800

    updata

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 $(function(){
 	var width = $(window).width(),
 		height = $(window).height();
+	if ( width >= 768 ) {
+		$("header").css({
+			"height": ((width * 9) / 16)
+		})
+	}
 	$(".function--playFullScreen").on("click", function () {
 		$(".black").fadeIn();
 		$(".lightbox--video").fadeIn();
@@ -12,5 +17,8 @@ $(function(){
 	$(".black").on("click", function () {
 		$(this).fadeOut();
 		$(this).siblings(".lightbox").fadeOut();
+	});
+	$(".storage__open").on("click", function () {
+		$(this).parent().toggleClass("active");
 	});
 });
