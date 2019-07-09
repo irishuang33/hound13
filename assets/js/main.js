4ed73d56297212e9e25b19c9258db2248c39f4d3
yutinghCW <33119123+yutinghCW@users.noreply.github.com>
2019-07-09 19:45:07 +0800

    new--frame

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,4 +21,62 @@ $(function(){
 	$(".storage__open").on("click", function () {
 		$(this).parent().toggleClass("active");
 	});
+
+	// slides
+	$(".slider").each(function () {
+		var $slider = $(this),
+			$slider_wrap = $slider.children(".slider-wrap"),
+			$slider_navi_prev = $(this).children(".slider-navi").children(".slider-navi--prev"),
+			$slider_navi_next = $(this).children(".slider-navi").children(".slider-navi--next"),
+			$slider_item = $slider_wrap.children(".slider-item"),
+			slider_item_width = $(this).parent().outerWidth(), //每張slide寬度
+			slider_count = $slider_item.length,
+			slider_item_index = 0, //預宣告slide為0
+			index = 0;
+
+		if ( $slider.hasClass("slider-lr") ) {
+			$slider_wrap.css({
+				"width": slider_item_width * (slider_count + 1),
+			});
+			$slider_item.css({
+				"width": slider_item_width
+			});
+		}
+		// $slider_item.first().clone().css({
+		// 	"width": slider_item_width
+		// }).appendTo($slider_wrap);
+		console.log(slider_item_width);
+		function switch_next() {
+			if ($slider_wrap.is(":animated")) return;
+			$slider_wrap.animate({left: "-="+slider_item_width}, function() {
+				if (index >= slider_count - 1) {
+					index = -1;
+					$(this).css("left", 0);
+				}
+				index++;
+				switch_dot();
+			});
+		}
+		function switch_dot() {
+		}
+		function switch_prev() {
+			if ($slider_wrap.is(":animated")) return;
+			if (index <= 0) {
+				index = slider_count;
+				$slider_wrap.css("left", -(index * slider_item_width));
+			}
+			$slider_wrap.animate({left: "+="+slider_item_width}, function() {
+				index--;
+				switch_dot();
+			});
+		}
+		$slider_navi_next.click(switch_next);
+		$slider_navi_prev.click(switch_prev);
+		// var sid = setInterval(switch_next, 2000);
+		// $slider.hover(function() {
+		// 	clearInterval(sid);
+		// },function() {
+		// 	sid = setInterval(switch_next, 2000);
+		// });
+	})
 });
