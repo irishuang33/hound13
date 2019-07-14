d0b9facb9d68a12c5a6fc143dc6020756bc11b34
hwangiris <hwangukjeong@gmail.com>
2019-07-14 22:08:53 +0800

    minimize

--- assets/config.rb
+++ assets/config.rb
@@ -14,7 +14,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
@@ -28,4 +28,4 @@ line_comments = false
 # and then run:
 
 Encoding.default_external = "utf-8"
-sourcemap = true
+# sourcemap = true
